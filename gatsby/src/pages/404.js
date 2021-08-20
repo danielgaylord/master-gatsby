0baaa61d96028ad3dc7ef1bf5a5c6eb8fe41a71a
Daniel Gaylord <danielgaylord@gmail.com>
2021-08-20 16:38:40 -0400

    Module 2 - Video 5

--- gatsby/src/pages/404.js
+++ gatsby/src/pages/404.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from '../components/Nav';
 
 export default function FourOhFourPage() {
   return (
     <div>
+      <Nav />
       <p>Hey - That page doesn't exist!!!</p>
     </div>
   );
