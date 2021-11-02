7a0136d150ddf5f7f3827c0fd4f57860f4bb833e
Daniel Gaylord <danielgaylord@gmail.com>
2021-11-02 14:29:41 -0400

    Update PizzaList.js

--- gatsby/src/components/PizzaList.js
+++ gatsby/src/components/PizzaList.js
@@ -4,9 +4,9 @@ import { Link } from 'gatsby';
 function SinglePizza({ pizza }) {
   return (
     <div>
-      <Link to={`/pizza/${pizza.slug.current}`} >
+      <Link to={`/pizza/${pizza.slug.current}`}>
         <h2>
-            <span className="mark">{pizza.name}</span>
+          <span className="mark">{pizza.name}</span>
         </h2>
         <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
       </Link>
