d0a46a774d3543c340d75b05081259cadb6cbad2
Daniel Gaylord <danielgaylord@gmail.com>
2021-11-02 14:28:57 -0400

    Module 5 Video 18

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
   return (
     <>
-      <p>Hey! I'm the Pizzas page</p>
+      <PizzaList pizzas = {data.pizzas.nodes} />
     </>
   );
 }
+
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        price
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
