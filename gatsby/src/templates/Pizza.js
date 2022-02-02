3f72d173bf19d3462b37269c5011f76e12c98017
Dan Gaylord <danielgaylord@gmail.com>
2022-02-02 11:45:40 -0500

    Module 7 video 24
    Templating and styling the single pizza page

--- gatsby/src/templates/Pizza.js
+++ gatsby/src/templates/Pizza.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import styled from 'styled-components';
 
-export default function SinglePizzaPage() {
-    return <p>Single Pizza!!!</p>
+const PizzaGrid = styled.div`
+    display: grid;
+    grid-gap: 2rem;
+    grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+`;
+
+export default function SinglePizzaPage({ data }) {
+    const { pizza } = data;
+    const imageAsset = getImage(pizza.image.asset);
+    return (
+        <PizzaGrid>
+            <GatsbyImage image={imageAsset} alt={pizza.name} />
+            <div>
+                <h2 className="mark">{pizza.name}</h2>
+                <ul>
+                    {pizza.toppings.map((topping) => (
+                        <li key={topping.id}>{topping.name}</li>
+                    ))}
+                </ul>
+            </div>
+        </PizzaGrid>
+    );
 }
 
 export const query = graphql`
